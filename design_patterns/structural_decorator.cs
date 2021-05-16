f3637c4f352bb5fed661e56e6b8a45d6fe79cbe7
simongarisch <gatman946@gmail.com>
2021-05-16 19:08:56 +1000

    typo

--- design_patterns/structural_decorator.cs
+++ design_patterns/structural_decorator.cs
@@ -22,7 +22,7 @@ class LogDoubler : ICalculation {
     }
 
     public int Calc(int input) {
-        Console.WriteLine("Starting the executioin with integer " + input);
+        Console.WriteLine("Starting the execution with integer " + input);
         int result = doubler.Calc(input);
         Console.WriteLine("Execution has completed with the result " + result);
         return result;
@@ -35,7 +35,7 @@ class Program {
         var logDoubler = new LogDoubler(new Doubler());
         logDoubler.Calc(2);
         /*
-        Starting the executioin with integer 2
+        Starting the execution with integer 2
         Execution has completed with the result 4
          */
     }
