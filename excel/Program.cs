b76bb73f6f7c6eeaa1c0ee008180de02f7ce4279
simongarisch <gatman946@gmail.com>
2021-05-21 16:03:04 +1000

    excel

---
+++ excel/Program.cs
@@ -0,0 +1,13 @@
+﻿// dotnet add package ExcelDna.AddIn --version 1.1.1
+// https://excel-dna.net/
+// https://github.com/Excel-DNA/ExcelDna/issues/175
+// Doesn't look like this supports dotnet core...
+using ExcelDna.Integration;
+
+
+public static class MyFunctions {
+    [ExcelFunction(Description = "My first .NET function")]
+    public static string SayHello(string name) {
+        return "Hello " + name;
+    }
+}
