75bce3364de7f14bd22cdf45544e5e3a2e5addee
simongarisch <gatman946@gmail.com>
2021-05-23 16:30:17 +1000

    example usage of excel dna

---
+++ ExcelDnaTest/ExcelDnaTest/Program.cs
@@ -0,0 +1,19 @@
+﻿// PM> Install-Package ExcelDna.AddIn
+using System;
+using ExcelDna.Integration;
+
+
+
+public static class MyFunctions {
+    [ExcelFunction(Description = "My first .NET function")]
+    public static string SayHello(string name) {
+        return "Hello, " + name;
+    }
+}
+
+
+class Program {
+    public static void Main() {
+        Console.WriteLine("Should be good to go...");
+    }
+}
