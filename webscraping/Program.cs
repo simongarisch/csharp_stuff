32ccb1ed3a5d603681602bb0f18c6b9cf1bb21eb
simongarisch <gatman946@gmail.com>
2021-05-26 07:16:59 +1000

    adding scraping example

---
+++ webscraping/Program.cs
@@ -0,0 +1,24 @@
+﻿// dotnet add package HtmlAgilityPack --version 1.11.33
+using System;
+using HtmlAgilityPack;
+
+class Program {
+    static void Main(string[] args) {
+        var web = new HtmlAgilityPack.HtmlWeb();
+        string url = "https://www.nuget.org/packages/HtmlAgilityPack/";
+        
+        try {
+            var doc = web.Load(url);
+            var tables = doc.DocumentNode.SelectNodes("//table");
+            foreach(HtmlNode table in tables) {
+                Console.WriteLine("Found: " + table.Id);
+                Console.WriteLine(table.InnerText);
+            }
+        } catch (System.Net.WebException ex){
+            Console.WriteLine("Nothing to see here...");
+            Console.WriteLine(ex.ToString()); // or log it somewhere...
+            return;
+        }
+
+    }
+}
