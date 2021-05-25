478e21dc201575076fd6de455a55fd4e5f059b46
simongarisch <gatman946@gmail.com>
2021-05-25 10:22:42 +1000

    adding tts example

---
+++ tts/tts/Program.cs
@@ -0,0 +1,14 @@
+﻿// doesn't appear to be supported in .NET core yet
+// Go to References -> Add Reference... -> Assemblies / Framework -> System.Speech
+using System;
+using System.Speech.Synthesis;
+
+class Program
+{
+    public static void Main()
+    {
+        var synthesizer = new SpeechSynthesizer();
+        synthesizer.SetOutputToDefaultAudioDevice();
+        synthesizer.Speak("The quick brown fox jumped over the lazy dog");
+    }
+}
