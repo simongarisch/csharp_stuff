9f3dbcfea7985d31e7a20391ec37bef3c7fec53c
simongarisch <gatman946@gmail.com>
2021-05-26 14:09:41 +1000

    applying dotnet format

--- webscraping/Program.cs
+++ webscraping/Program.cs
@@ -2,19 +2,25 @@
 using System;
 using HtmlAgilityPack;
 
-class Program {
-    static void Main(string[] args) {
+class Program
+{
+    static void Main(string[] args)
+    {
         var web = new HtmlAgilityPack.HtmlWeb();
         string url = "https://www.nuget.org/packages/HtmlAgilityPack/";
-        
-        try {
+
+        try
+        {
             var doc = web.Load(url);
             var tables = doc.DocumentNode.SelectNodes("//table");
-            foreach(HtmlNode table in tables) {
+            foreach (HtmlNode table in tables)
+            {
                 Console.WriteLine("Found: " + table.Id);
                 Console.WriteLine(table.InnerText);
             }
-        } catch (System.Net.WebException ex){
+        }
+        catch (System.Net.WebException ex)
+        {
             Console.WriteLine("Nothing to see here...");
             Console.WriteLine(ex.ToString()); // or log it somewhere...
             return;
