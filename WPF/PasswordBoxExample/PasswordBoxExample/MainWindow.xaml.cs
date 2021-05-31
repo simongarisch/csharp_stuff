490d9f97dea90aa06b3d9d96b31b08869406bcd9
simongarisch <gatman946@gmail.com>
2021-05-31 11:07:47 +1000

    adding a password box example

---
+++ WPF/PasswordBoxExample/PasswordBoxExample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordBoxExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            // MessageBox.Show("You clicked on the button");
+            string password = UiPasswordBox.Password;
+            MessageBox.Show($"Your password is '{password}'");
+        }
+    }
+}
