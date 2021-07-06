ab6999cabb650ee02915dcdaafbdd31b89d98a64
simongarisch <gatman946@gmail.com>
2021-07-06 13:05:05 +1000

    adding basic web service

---
+++ complete_csharp/BasicWebApplication/BasicWebApplication/BasicWebService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using Newtonsoft.Json;
+
+
+namespace BasicWebApplication
+{
+    /// <summary>
+    /// Summary description for BasicWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class BasicWebService : System.Web.Services.WebService
+    {
+        DataTable dtCountries = new DataTable();
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public string Countries()
+        {
+            dtCountries.Columns.Add("Country Name");
+            dtCountries.Columns.Add("Continent");
+
+            dtCountries.Rows.Add("India", "Asia");
+            dtCountries.Rows.Add("Germany", "Europe");
+            dtCountries.Rows.Add("Australia", "Australia");
+            return JsonConvert.SerializeObject(dtCountries);
+        }
+    }
+}
