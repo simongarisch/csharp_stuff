6467254794bd65d2190e6fd2d1188045c74ae183
simongarisch <gatman946@gmail.com>
2021-07-07 17:25:40 +1000

    adding a basic client app to read from the service

---
+++ complete_csharp/BasicClientApplication/BasicClientApplication/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace BasicClientApplication
+{
+    public partial class MainForm : Form
+    {
+        MyWebReference.BasicWebService proxy = new MyWebReference.BasicWebService();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string countriesJson = proxy.Countries();
+            DataTable dtCountries = JsonConvert.DeserializeObject<DataTable>(countriesJson);
+            dataGridView1.DataSource = dtCountries;
+        }
+    }
+}
