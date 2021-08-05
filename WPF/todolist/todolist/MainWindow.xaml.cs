8b268247532109adfe6bf1f9449ba9888bdd8116
simongarisch <gatman946@gmail.com>
2021-08-06 00:04:34 +1000

    adding todo example

---
+++ WPF/todolist/todolist/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+
+namespace todolist
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNewTodo(object sender, RoutedEventArgs e)
+        {
+            string content = NewItemText.Text;
+            if (string.IsNullOrEmpty(content))
+            {
+                MessageBox.Show("Please enter some text for the TODO");
+                return;
+            }
+
+            var item = new SingleItem(content);
+            TodoStack.Children.Add(item);
+        }
+    }
+}
---
+++ WPF/todolist/todolist/SingleItem.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace todolist
+{
+    public partial class SingleItem : UserControl
+    {
+        private string _content;
+
+        public SingleItem(string content)
+        {
+            _content = content;
+            if (string.IsNullOrEmpty(_content))
+            {
+                string[] options = new string[] { "Do the dishes", "Cook some bacon", "Practice more WPF", "Browse Reddit" };
+                int index = new Random().Next(0, options.Count());
+                _content = options[index];
+            }
+
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(OnLoaded);
+        }
+
+        public SingleItem(): this(null)
+        {
+        }
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TodoLabel.Content = _content;
+        }
+
+        private void Remove(object sender, MouseButtonEventArgs e)
+        {
+            ((Panel)this.Parent).Children.Remove(this);
+        }
+    }
+}
